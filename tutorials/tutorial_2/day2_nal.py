ffad156feaff946f670b7f919c7b2eee7ba98878
NaingAungLwin272 <nal27201@gmail.com>
2022-04-21 21:36:15 +0630

    ###

---
+++ tutorials/tutorial_2/day2_nal.py
@@ -0,0 +1,22 @@
+# 1
+x = 4
+y = 6
+z = x * y
+print(z)
+
+# 2
+greet = "Hello"
+syntax = "This is Day2 Assignment."
+print(greet + ', ' + syntax)
+
+
+# 3
+list = [1, 2, 3, 4, 5]
+result = slice(1, 4)
+print(list[result])
+
+
+# 4
+price = 100
+syntax = "My pen`s price is {}."
+print(syntax.format(price))
