1cd34763dd56e61f48dc0722d8e63d4b06e964e8
NaingAungLwin272 <nal27201@gmail.com>
2022-04-22 20:17:09 +0630

    #4

---
+++ tutorials/tutorial_4/day4_nal.py
@@ -0,0 +1,23 @@
+#   1
+L = [*range(0, 11)]
+print(L)
+square_list = map(lambda x: x ** 2, L)
+print(list(square_list))
+cube_list = map(lambda x: x ** 3, L)
+print(list(cube_list))
+
+
+#   2
+n = 5
+for i in range(0, n):
+    for j in range(0, i+1):
+        print("* ", end="")
+
+    print("\r")
+
+n = 5
+for i in range(0, n):
+    for j in range(0, i+1):
+        print("? ", end="")
+
+    print("\r")
