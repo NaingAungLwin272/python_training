41b4b647f945c87005518b9e1e4f57d626b9b028
NaingAungLwin272 <nal27201@gmail.com>
2022-04-25 22:06:38 +0630

    #Create Square list and Cube list with lambda method and print stars pattern

--- tutorials/tutorial_4/day4_nal.py
+++ tutorials/tutorial_4/day4_nal.py
@@ -9,9 +9,8 @@ print(list(cube_list))
 
 #   2
 def star_function(x):
-    n = 5
-    for i in range(0, n):
-        for j in range(0, i+1):
+    for i in range(5):
+        for j in range(i+1):
             print(x, end="")
 
         print("\r")
