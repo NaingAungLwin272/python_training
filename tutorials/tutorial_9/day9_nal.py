9c77abd2b0c5e456475c1682d049f265d8c8d790
NaingAungLwin272 <nal27201@gmail.com>
2022-05-02 11:48:08 +0630

    # 9 Calculator Assignment

--- tutorials/tutorial_9/day9_nal.py
+++ tutorials/tutorial_9/day9_nal.py
@@ -15,41 +15,41 @@ while True:
     user_input = str(input("Enter Choice : "))
 
     if user_input.lower() in "add" or user_input == "1":
-        num1 = int(input("Enter First Number : "))
-        num2 = int(input("Enter Second Number: "))
+        num1 = float(input("Enter First Number : "))
+        num2 = float(input("Enter Second Number: "))
         result = num1 + num2
         print(" {} + {} ".format(num1, num2), "= ", result)
     elif user_input.lower() in "subtract" or user_input == "2":
-        num1 = int(input("Enter First Number : "))
-        num2 = int(input("Enter Second Number: "))
+        num1 = float(input("Enter First Number : "))
+        num2 = float(input("Enter Second Number: "))
         result = num1 - num2
         print(" {} - {} ".format(num1, num2), "= ", result)
     elif user_input.lower() in "multiply" or user_input == "3":
-        num1 = int(input("Enter First Number : "))
-        num2 = int(input("Enter Second Number: "))
+        num1 = float(input("Enter First Number : "))
+        num2 = float(input("Enter Second Number: "))
         result = num1 * num2
         print(" {} * {} ".format(num1, num2), "= ", result)
     elif user_input.lower() in "divided" or user_input == "4":
-        num1 = int(input("Enter First Number : "))
-        num2 = int(input("Enter Second Number: "))
+        num1 = float(input("Enter First Number : "))
+        num2 = float(input("Enter Second Number: "))
         result = num1 / num2
         print(" {} / {} ".format(num1, num2), "= ", result)
     elif user_input.lower() in "power" or user_input == "5":
-        num1 = int(input("Enter First Number : "))
-        num2 = int(input("Enter Second Number: "))
+        num1 = float(input("Enter First Number : "))
+        num2 = float(input("Enter Second Number: "))
         result = math.pow(num1, num2)
         print(" {} ** {} ".format(num1, num2), "= ", result)
     elif user_input.lower() in "modulo" or user_input == "6":
-        num1 = int(input("Enter First Number : "))
-        num2 = int(input("Enter Second Number: "))
+        num1 = float(input("Enter First Number : "))
+        num2 = float(input("Enter Second Number: "))
         result = num1 % num2
         print(" {} % {} ".format(num1, num2), "= ", result)
     elif user_input.lower() in "squareroot" or user_input == "7":
-        num1 = int(input("Enter First Number : "))
+        num1 = float(input("Enter Number : "))
         result = math.sqrt(num1)
         print(" Square root of {} ".format(num1), "= ", result)
     elif user_input.lower() in "cube" or user_input == "8":
-        num1 = int(input("Enter First Number : "))
+        num1 = float(input("Enter Number : "))
         result = num1 ** 3
         print("Cube of {} ".format(num1), "= ", result)
     else:
