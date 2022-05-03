e192a48556d1cedb8373fcb5bee192df296a2385
NaingAungLwin272 <nal27201@gmail.com>
2022-05-03 22:39:03 +0630

    #9 Calculator Assignment

--- tutorials/tutorial_9/day9_nal.py
+++ tutorials/tutorial_9/day9_nal.py
@@ -3,78 +3,81 @@ import math
 
 
 def calculate():
-    print("""Select Operation
-  1.Add
-  2.Subtract
-  3.Multiply
-  4.Divide
-  5.Power
-  6.Modulo
-  7.Square_Root
-  8.Cube
-  -----------------------------------------------------------""")
-    user_input = input("Enter User Choice : ")
+    while True:
 
-    try:
-        if user_input.lower() in "squareroot" or user_input == "7":
-            num1 = float(input('Enter Number : '))
+        print("""Select Operation
+                1.Add
+                2.Subtract
+                3.Multiply
+                4.Divide
+                5.Power
+                6.Modulo
+                7.Square_Root
+                8.Cube
+                -----------------------------------------------------------""")
 
-        elif user_input.lower() in "cube" or user_input == "8":
-            num1 = float(input('Enter Number : '))
+        user_input = str(input("Enter User Input : "))
 
+        try:
+            if user_input.lower() == "squareroot" or user_input == "7":
+                num1 = float(input('Enter Number : '))
+
+            elif user_input.lower() == "cube" or user_input == "8":
+                num1 = float(input('Enter Number : '))
+
+            else:
+                num1 = float(input('Enter First Number : '))
+                num2 = float(input('Enter Second Number : '))
+        except ValueError:
+            print("Check your Input Again")
         else:
-            num1 = float(input('Enter First Number : '))
-            num2 = float(input('Enter Second Number : '))
-    except ValueError:
-        print("Check your number input and userchoice again")
+            if user_input.lower() == "add" or user_input == "1":
 
-    else:
+                result = num1 + num2
+                print(" {} + {} ".format(num1, num2), "= ", result)
+            elif user_input.lower() == "subtract" or user_input == "2":
+                result = num1 - num2
+                print(" {} - {} ".format(num1, num2), "= ", result)
+            elif user_input.lower() == "multiply" or user_input == "3":
 
-        if user_input.lower() in "add" or user_input == "1":
+                result = num1 * num2
+                print(" {} * {} ".format(num1, num2), "= ", result)
+            elif user_input.lower() == "divided" or user_input == "4":
 
-            result = num1 + num2
-            print(" {} + {} ".format(num1, num2), "= ", result)
-        elif user_input.lower() in "subtract" or user_input == "2":
-            result = num1 - num2
-            print(" {} - {} ".format(num1, num2), "= ", result)
-        elif user_input.lower() in "multiply" or user_input == "3":
+                result = num1 / num2
+                print(" {} / {} ".format(num1, num2), "= ", result)
+            elif user_input.lower() == "power" or user_input == "5":
 
-            result = num1 * num2
-            print(" {} * {} ".format(num1, num2), "= ", result)
-        elif user_input.lower() in "divided" or user_input == "4":
+                result = math.pow(num1, num2)
+                print(" {} ** {} ".format(num1, num2), "= ", result)
+            elif user_input.lower() == "modulo" or user_input == "6":
+                result = num1 % num2
+                print(" {} % {} ".format(num1, num2), "= ", result)
+            elif user_input.lower() == "squareroot" or user_input == "7":
 
-            result = num1 / num2
-            print(" {} / {} ".format(num1, num2), "= ", result)
-        elif user_input.lower() in "power" or user_input == "5":
+                result = math.sqrt(num1)
+                print(" Square root of {} ".format(num1), "= ", result)
+            elif user_input.lower() == "cube" or user_input == "8":
 
-            result = math.pow(num1, num2)
-            print(" {} ** {} ".format(num1, num2), "= ", result)
-        elif user_input.lower() in "modulo" or user_input == "6":
-            result = num1 % num2
-            print(" {} % {} ".format(num1, num2), "= ", result)
-        elif user_input.lower() in "squareroot" or user_input == "7":
+                result = num1 ** 3
+                print("Cube of {} ".format(num1), "= ", result)
+            else:
+                print("Check your Input Again")
+                continue
 
-            result = math.sqrt(num1)
-            print(" Square root of {} ".format(num1), "= ", result)
-        elif user_input.lower() in "cube" or user_input == "8":
-
-            result = num1 ** 3
-            print("Cube of {} ".format(num1), "= ", result)
-        else:
-            print("Invalid Input")
-
-    # Add again() function to calculate() function
-    again()
+            again()
+            break
 
 
 def again():
 
     user_choice = input("Let`s do next calculation?(yes/no): ")
 
-    if user_choice.lower() in "yes":
+    if user_choice.lower() == "yes":
         calculate()
-    elif user_choice.lower() in "no":
+    elif user_choice.lower() == "no":
         print("Bye Bye")
+
     else:
         print("Invalid Input")
         again()
