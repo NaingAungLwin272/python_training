afd346774e4e23dea53237962fd4bbae56c3e39f
NaingAungLwin272 <nal27201@gmail.com>
2022-05-04 10:23:22 +0630

    # 9 Calculator Assignment

--- tutorials/tutorial_9/day9_nal.py
+++ tutorials/tutorial_9/day9_nal.py
@@ -18,55 +18,156 @@ def calculate():
 
         user_input = str(input("Enter User Input : "))
 
-        try:
-            if user_input.lower() == "squareroot" or user_input == "7":
-                num1 = float(input('Enter Number : '))
+        if user_input.lower() == "add" or user_input == "1":
 
-            elif user_input.lower() == "cube" or user_input == "8":
-                num1 = float(input('Enter Number : '))
+            while True:
+                try:
+                    num1 = float(input("Enter First Number : "))
+                    while True:
+                        try:
+                            num2 = float(input("Enter Second Number : "))
+                            break
+                        except ValueError:
+                            print("Valid number pls")
+                            print("---------------------------------------")
 
-            else:
-                num1 = float(input('Enter First Number : '))
-                num2 = float(input('Enter Second Number : '))
-        except ValueError:
-            print("Check your Input Again")
-        else:
-            if user_input.lower() == "add" or user_input == "1":
+                    result = num1 + num2
+                    print(" {} + {} ".format(num1, num2), "= ", result)
+                    break
+                except ValueError:
+                    print("Valid number pls")
+                    print("---------------------------------------")
 
-                result = num1 + num2
-                print(" {} + {} ".format(num1, num2), "= ", result)
-            elif user_input.lower() == "subtract" or user_input == "2":
-                result = num1 - num2
-                print(" {} - {} ".format(num1, num2), "= ", result)
-            elif user_input.lower() == "multiply" or user_input == "3":
+        elif user_input.lower() == "subtract" or user_input == "2":
+            while True:
+                try:
+                    num1 = float(input("Enter First Number : "))
+                    while True:
+                        try:
+                            num2 = float(input("Enter Second Number : "))
+                            break
+                        except ValueError:
+                            print("Valid number pls")
+                            print("---------------------------------------")
 
-                result = num1 * num2
-                print(" {} * {} ".format(num1, num2), "= ", result)
-            elif user_input.lower() == "divided" or user_input == "4":
+                    result = num1 - num2
+                    print(" {} + {} ".format(num1, num2), "= ", result)
+                    break
+                except ValueError:
+                    print("Valid number pls")
+                    print("---------------------------------------")
 
-                result = num1 / num2
-                print(" {} / {} ".format(num1, num2), "= ", result)
-            elif user_input.lower() == "power" or user_input == "5":
+        elif user_input.lower() == "multiply" or user_input == "3":
+            while True:
+                try:
+                    num1 = float(input("Enter First Number : "))
+                    while True:
+                        try:
+                            num2 = float(input("Enter Second Number : "))
+                            break
+                        except ValueError:
+                            print("Valid number pls")
+                            print("---------------------------------------")
+
+                    result = num1 * num2
+                    print(" {} + {} ".format(num1, num2), "= ", result)
+                    break
+                except ValueError:
+                    print("Valid number pls")
+                    print("---------------------------------------")
+        elif user_input.lower() == "divided" or user_input == "4":
+
+            while True:
+                try:
+                    num1 = float(input("Enter First Number : "))
+                    while True:
+                        try:
+                            num2 = float(input("Enter Second Number : "))
+                            break
+                        except ValueError:
+                            print("Valid number pls")
+                            print("---------------------------------------")
+
+                    result = num1 / num2
+                    print(" {} / {} ".format(num1, num2), "= ", result)
+                    break
+                except ValueError:
+                    print("Valid number pls")
+                    print("---------------------------------------")
+
+        elif user_input.lower() == "power" or user_input == "5":
+
+            try:
+                num1 = float(input("Enter First Number : "))
+                while True:
+                    try:
+                        num2 = float(input("Enter Second Number : "))
+                        break
+                    except ValueError:
+                        print("Valid number pls")
+                        print("---------------------------------------")
 
                 result = math.pow(num1, num2)
                 print(" {} ** {} ".format(num1, num2), "= ", result)
-            elif user_input.lower() == "modulo" or user_input == "6":
+                break
+            except ValueError:
+                print("Valid number pls")
+                print("---------------------------------------")
+
+        elif user_input.lower() == "modulo" or user_input == "6":
+
+            try:
+                num1 = float(input("Enter First Number : "))
+                while True:
+                    try:
+                        num2 = float(input("Enter Second Number : "))
+                        break
+                    except ValueError:
+                        print("Valid number pls")
+                        print("---------------------------------------")
+
                 result = num1 % num2
                 print(" {} % {} ".format(num1, num2), "= ", result)
-            elif user_input.lower() == "squareroot" or user_input == "7":
+                break
+            except ValueError:
+                print("Valid number pls")
+                print("---------------------------------------")
 
-                result = math.sqrt(num1)
-                print(" Square root of {} ".format(num1), "= ", result)
-            elif user_input.lower() == "cube" or user_input == "8":
+        elif user_input.lower() == "squareroot" or user_input == "7":
+            while True:
+                try:
 
-                result = num1 ** 3
-                print("Cube of {} ".format(num1), "= ", result)
-            else:
-                print("Check your Input Again")
-                continue
+                    num1 = float(input('Enter Number : '))
+                    break
 
-            again()
-            break
+                except ValueError:
+                    print("Valid number pls")
+                    print("---------------------------------------")
+
+            result = math.sqrt(num1)
+            print(" Square root of {} ".format(num1), "= ", result)
+
+        elif user_input.lower() == "cube" or user_input == "8":
+
+            while True:
+                try:
+
+                    num1 = float(input('Enter Number : '))
+                    break
+
+                except ValueError:
+                    print("Valid number pls")
+                    print("---------------------------------------")
+
+            result = num1 ** 3
+            print("Cube of {} ".format(num1), "= ", result)
+        else:
+            print("Check your UserChoice again")
+            print("---------------------------------------")
+            continue
+
+        again()
+        break
 
 
 def again():
@@ -77,9 +178,11 @@ def again():
         calculate()
     elif user_choice.lower() == "no":
         print("Bye Bye")
+        print("---------------------------------------")
 
     else:
         print("Invalid Input")
+        print("---------------------------------------")
         again()
 
 
