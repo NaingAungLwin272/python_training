f044337a578a47f20dd679d0a3af70780d512e25
NaingAungLwin272 <84072000+NaingAungLwin272@users.noreply.github.com>
2022-05-08 22:57:15 +0630

    # 12 Clock Up,Clock Down,Timer Assignment

--- tutorials/tutroial_12/day12_nal.py
+++ tutorials/tutroial_12/day12_nal.py
@@ -10,66 +10,83 @@ def count_time():
 
         while user_choice != '1' and user_choice != '2':
             print(" ")
-            user_choice = input("Choose only 1 or 2.Input Again Please : ")
+            user_choice = input(" 1 For CountUp or 2 For CountDown : ")
             print(" ")
 
         if user_choice == '1':
             while True:
                 try:
-                    start = int(input("Enter Start Time :"))
+                    start = int(input("Enter Start Time(sec) :"))
                     print(" ")
                     while True:
                         try:
-                            end = int(input("Enter End time : "))
+                            end = int(input("Enter End Time(sec) : "))
                             print(" ")
                             break
                         except ValueError:
 
-                            print("Check your EndTime Input Again")
+                            print("Invalid Time Input.Please Enter A Valid One.")
                             print("---------------------------------------")
 
-                    for x in range(start, end):
-                        timer = '{:02d}:{:02d}:{:02d}'.format(0, 0, x)
-                        print(timer)
-                        sleep(1)
-                    print("Time Up")
-                    print(" ")
-                    break
+                    if end < start:
+                        print("End Time must be Greater than Start time")
+                    else:
+                        for x in range(start, end):
+
+                            hour = x // 3600
+                            x %= 3600
+                            mins = x // 60
+                            secs = x % 60
+                            timer = '{:02d}:{:02d}:{:02d}'.format(
+                                hour, mins, secs)
+                            print(timer)
+                            sleep(1)
+                        print("Time Up")
+                        print(" ")
+                        break
                 except ValueError:
 
-                    print("Check your StartTime Input again")
+                    print("Invalid Time Input.Please Enter A Valid One.")
                     print("---------------------------------------")
 
         elif user_choice == '2':
             while True:
                 try:
-                    start = int(input("Enter Start Time :"))
+                    start = int(input("Enter Start Time(sec) :"))
                     print(" ")
                     while True:
                         try:
-                            end = int(input("Enter End time : "))
+                            end = int(input("Enter End time(sec) : "))
                             print(" ")
                             break
                         except ValueError:
 
-                            print("Check your EndTime Input Again")
+                            print("Invalid Time Input.Please Enter A Valid One.")
                             print("---------------------------------------")
 
-                    for x in range(start, end, -1):
-                        timer = '{:02d}:{:02d}:{:02d}'.format(0, 0, x)
-                        print(timer)
-                        sleep(1)
-                    print("Time Up")
-                    print(" ")
-                    break
+                    if end > start:
+                        print("End time must be Less than Start time")
+                    else:
+                        for x in range(start, end, -1):
+                            hour = x // 3600
+                            x %= 3600
+                            mins = x // 60
+                            secs = x % 60
+                            timer = '{:02d}:{:02d}:{:02d}'.format(
+                                hour, mins, secs)
+                            print(timer)
+                            sleep(1)
+                        print("Time Up")
+                        print(" ")
+                        break
                 except ValueError:
 
-                    print("Check your StartTime Input again")
+                    print("Invalid Time Input.Please Enter A Valid One.")
                     print("---------------------------------------")
 
         again = input("Restart again?(Y or N) : ")
         while again.lower() != 'y' and again.lower() != 'n':
-            again = input("Chosse only Y or N.Input again Please : ")
+            again = input("Invalid Input.Input Y or N only : ")
             print(" ")
             continue
 
