bf518ceafee79334e1ea0dcae19f9f694b676e55
NaingAungLwin272 <84072000+NaingAungLwin272@users.noreply.github.com>
2022-05-15 03:55:23 +0630

    # 19 Flask Template and Static Files Assignment

---
+++ flasktutorials/flasktutorial_3/flasktuto_3.py
@@ -0,0 +1,26 @@
+
+from flask import Flask, render_template, request, redirect, url_for
+app = Flask(__name__)
+
+
+@app.route('/welcome')
+def welcome():
+    return 'Welcome'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        confirmpword = request.form['confirmpassword']
+        if len(username) == 0 or len(password) == 0 or len(email) == 0 or len(confirmpword) == 0:
+            error = 'Input Can`t be blank'
+        else:
+            return redirect(url_for('welcome'))
+    return render_template('index.html', error=error)
+
+
+app.run()
